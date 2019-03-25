50a314505b90bb71d7d8df0330a0f616a0987705
nebojsa <nebojsart1409@yahoo.com>
2019-03-25 17:28:29 +0100

    The Listview Tile

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget{
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc =StoriesProvider.of(context);
+
+    return null;
+  }
+}
